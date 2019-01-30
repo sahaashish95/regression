21b5b9eae74e4d599b17be3c838993b76cba3804
Your Name <sahaashish95@gmail.com>
2019-01-30 10:31:39 +0530

    third Version

---
+++ classification/Section 13 -------------------- Part 3 - Classification --------------------/all classification algo.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  3 08:36:57 2018
+
+@author: Ashish Saha
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset=pd.read_csv('Social_Network_Ads.csv')
+X=dataset.iloc[:,[2,3]].values
+y=dataset.iloc[:,4].values
+
+#from sklearn.model_selection import train_test_split
+#X_train, X_test, y_train, y_test= train_test_split(X, y)
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc= StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#logistic classifier
+'''from sklearn.linear_model import LogisticRegression
+classifier= LogisticRegression(random_state= 0)
+classifier.fit(X_train, y_train)'''
+
+#kneighbour classifier
+'''from sklearn.neighbors import KNeighborsClassifier
+classifier= KNeighborsClassifier(metric= 'minkowski', p= 2)
+classifier.fit(X_train, y_train)'''
+'''
+#supoort vector classifier with linear kernel
+from sklearn.svm import SVC
+classifier= SVC(kernel='linear', random_state= 0)
+classifier.fit(X_train, y_train)'''
+
+
+'''##supoort vector classifier with gaussian  kernel
+from sklearn.svm import SVC
+classifier= SVC(kernel='rbf', random_state= 0)
+classifier.fit(X_train, y_train)'''
+
+'''#naive bayes classifier
+from sklearn.naive_bayes import GaussianNB
+classifier=GaussianNB()
+classifier.fit(X_train, y_train)'''
+
+#decision tree classifier
+from sklearn.tree import DecisionTreeClassifier
+classifier=DecisionTreeClassifier(criterion='entropy' ,random_state=0)
+classifier.fit(X_train, y_train)
+
+y_pred= classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test, y_pred)
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
